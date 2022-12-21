bdaf7ad38049cd03c746ace147a54e36f1c4ce9e
2022F Advanced OS Student <luozhonghao@baker1.dsrg.utoronto.ca>
2022-12-21 17:58:30 -0500

    add test2 and test3

--- kafka-scalibility-test/kafka_test.py
+++ kafka-scalibility-test/kafka_test.py
@@ -28,65 +28,139 @@ class data_loader:
                 get_log(path)
             else:
                 for child in os.listdir(path):
+                    if self.target == 0:
+                        return
                     recur(os.path.join(path, child))
                     
         recur(os.path.join(os.path.dirname(__file__), 'json-logs'))
         return self.res
 
 class Tester:
-    def __init__(self, topic, num): 
+    def __init__(self, topic, records, producers, consumers): 
         self.topic = topic
-        self.num = num    
-        self.consume_time = 0
-        self.produce_time = 0
-        self.consume_start = None
+        self.r_num = records
+        self.p_num = producers
+        self.c_num = consumers
+        self.producers_time = [0 for _ in range(self.p_num)]
+        self.consumers_time = [0 for _ in range(self.c_num)]
     
     def run(self):
-        t1 = threading.Thread(target=self.produce)
-        t2 = threading.Thread(target=self.consume)
-        t2.start()
+        producer_threads = list()
+        consumer_threads = list()
+        for i in range(self.p_num):
+            producer_threads.append(threading.Thread(target=self.produce, args=[i]))
+        for i in range(self.c_num):
+            consumer_threads.append(threading.Thread(target=self.consume, args=[i]))
+        
+        for t in consumer_threads:
+            t.start()
         time.sleep(1)
-        t1.start()
-        t1.join()
-        t2.join()
-        return f'Total records : {self.num}, time for producer {self.produce_time}, time for consumer {self.consume_time}.'
+        for t in producer_threads:
+            t.start()
             
-    def produce(self):
+        for t in producer_threads:
+            t.join()
+        for t in consumer_threads:
+            t.join()
+
+        return self.producers_time, self.consumers_time
+            
+    def produce(self, seq):
         producer = KafkaProducer(bootstrap_servers='localhost:9092')
-        records = data_loader(self.num).get_data()
+        records = data_loader(self.r_num // self.p_num).get_data()
         for record in records:
             start = time.time()
             producer.send(self.topic, pickle.dumps(record))
-            self.produce_time += time.time() - start
+            self.producers_time[seq] += time.time() - start
 
-    def consume(self):
+    def consume(self, seq):
         consumer = KafkaConsumer(self.topic, 
                                 auto_offset_reset="latest",
-                                consumer_timeout_ms=5000,
+                                consumer_timeout_ms=2000,
                                 )
-        
+        total_time = 0
+        consume_start = None
         for record in consumer:
-            if self.consume_start != None:
-                self.consume_time += time.time() - self.consume_start
-            self.consume_start = time.time()
+            if consume_start != None:
+                total_time += time.time() - consume_start
+            consume_start = time.time()
+        self.consumers_time[seq] = total_time
         
-    
 def test1():
+    test_name = 'test1'
     x = []
-    y = []
+    yProduce = []
+    yConsume = []
     num = 10
-    while num < 1000:
-        tester = Tester('test1', num)
+    while num < 10000:
+        tester = Tester(test_name, num, 1, 1)
         x.append(num)
-        y.append(tester.run())
+        res = tester.run()
+        yProduce.append(sum(res[0]))
+        yConsume.append(sum(res[1]))
         num = int(num * 1.5)
-    plt.plot(x, y)
-    plt.savefig('test1.png')
+    
+    plt.clf()   
+    plt.title('1 producer and 1 consumer')
+    plt.xlabel('number of records')
+    plt.ylabel('time')
+    plt.plot(x, yProduce, label='time for producer')
+    plt.plot(x, yConsume, label='time for consumer')
+    plt.legend()
+    plt.savefig(f'{test_name}.png')
 
+def test2():
+    test_name = 'test2'
+    x = []
+    yProduce = []
+    yConsume = []
+    producers = 1
+    records= 10000
+    while producers < 9:
+        tester = Tester(test_name, records, producers, 1)
+        x.append(producers)
+        res = tester.run()
+        yProduce.append(sum(res[0]))
+        yConsume.append(sum(res[1]))
+        producers += 1
+    
+    plt.clf()
+    plt.title('10000 records and 1 consumer')
+    plt.xlabel('number of producers')
+    plt.ylabel('time')
+    plt.plot(x, yProduce, label='total time for producers')
+    plt.plot(x, yConsume, label='time for consumer')
+    plt.legend()
+    plt.savefig(f'{test_name}.png')
+
+def test3():
+    test_name = 'test3'
+    x = []
+    yProduce = []
+    yConsume = []
+    consumers = 1
+    records= 10000
+    while consumers < 9:
+        tester = Tester(test_name, records, 1, consumers)
+        x.append(consumers)
+        res = tester.run()
+        yProduce.append(sum(res[0]))
+        yConsume.append(sum(res[1]))
+        consumers += 1
+    
+    plt.clf()
+    plt.title('10000 records and 1 producer')
+    plt.xlabel('number of consumers')
+    plt.ylabel('time')
+    plt.plot(x, yProduce, label='time for producer')
+    plt.plot(x, yConsume, label='total time for consumers')
+    plt.legend()
+    plt.savefig(f'{test_name}.png')
 
 
 if __name__ == "__main__":
     test1()
-    
+    test2()
+    test3()
 
     
